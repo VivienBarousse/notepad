15d1572f2db75b1f6b9d4c9543cdbace0bcf2dc3
Vivien Barousse <barousse.vivien@gmail.com>
2013-12-07 18:25:08 +0000

    Move the Rectangle class in the lib/ folder

---
+++ lib/notepad.rb
@@ -0,0 +1,6 @@
+
+module Notepad
+  ROOT = File.expand_path("../../lib/", __FILE__)
+
+  require ROOT + '/notepad/rectangle'
+end
---
+++ lib/notepad/rectangle.rb
@@ -0,0 +1,40 @@
+module Notepad
+  class Rectangle
+
+    attr_accessor :top, :bottom, :left, :right
+    
+    def initialize(top, bottom, left, right)
+      @top = top
+      @bottom = bottom
+      @left = left
+      @right = right
+    end
+
+    def area
+      (@right - @left) * (@bottom - @top)
+    end
+
+    def intersect_area(other)
+      x = [0, [right, other.right].min - [left, other.left].max].max
+      y = [0, [bottom, other.bottom].min - [top, other.top].max].max
+      x * y
+    end
+
+    def inspect
+      [top, bottom, left, right].inspect
+    end
+
+    def ==(other)
+      [top, bottom, left, right] == [other.top, other.bottom, other.left, other.right]
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      top + bottom + left + right
+    end
+
+  end
+end
